421441679dd3c880cadcf9a37a528739573f1c9d
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-03-02 15:11:17 +0300

    Create Thunk

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,10 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
-import initialStateBooks from '../../components/initialStateBooks';
+import axios from 'axios';
+// import no-extraneous-dependencies
 
 const initialState = {
-  bookList: initialStateBooks,
+  bookList: [],
 };
+
+export const fetchBooksFromApi = createAsyncThunk(
+  'books/fetchBooksApi',
+  async () => {
+    const res = await axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/j8tKlQ66WOeSyXaA9k7E/books');
+    return res.data;
+  },
+);
+
+export const createNewBook = createAsyncThunk(
+  'books/createBook',
+  async (data) => {
+    const newBook = data;
+    newBook.item_id = uuidv4();
+    newBook.category = 'Action';
+    const res = await axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/j8tKlQ66WOeSyXaA9k7E/books', newBook);
+    return res.data;
+  },
+);
+
+export const deleteBook = createAsyncThunk(
+  'books/deleteBook',
+  async (data) => {
+    const newBook = {};
+    newBook.item_id = data;
+    newBook.category = 'Action';
+    const res = await axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/j8tKlQ66WOeSyXaA9k7E/books/${data}`, newBook);
+    return res.data;
+  },
+);
 
 export const booksSlice = createSlice({
   name: 'books',
